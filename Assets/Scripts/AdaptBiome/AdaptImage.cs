a656723372a4182e15d4040e53302f85995b20a3
Fromiel <79905976+Fromiel@users.noreply.github.com>
2022-11-19 16:37:22 -0500

    build

--- Assets/Scripts/AdaptBiome/AdaptImage.cs
+++ Assets/Scripts/AdaptBiome/AdaptImage.cs
@@ -3,12 +3,7 @@ using UnityEngine.UI;
 
 public class AdaptImage : MonoBehaviour
 {
-
-    [SerializeField] private GameObject orange;
-    [SerializeField] private GameObject vert;
-    [SerializeField] private GameObject bleu;
-    [SerializeField] private GameObject violet;
-    [SerializeField] private RuleTile ruleTile;
+    
     private Color colorBiome;
     // Start is called before the first frame update
     void Start()
@@ -17,19 +12,15 @@ public class AdaptImage : MonoBehaviour
         {
             case 0:
                 colorBiome = new Color(0.76f, 0.12f, 0.22f);
-                ruleTile.m_DefaultGameObject = orange;
                 break;
             case 1:
                 colorBiome =new Color(0.76f, 0.12f, 0.22f);
-                ruleTile.m_DefaultGameObject = vert;
                 break;
             case 2:
                 colorBiome = new Color(0.76f, 0.12f, 0.22f);
-                ruleTile.m_DefaultGameObject = bleu;
                 break;
             case 3:
                 colorBiome = new Color(0.76f, 0.12f, 0.22f);
-                ruleTile.m_DefaultGameObject = violet;
                 break;
         }
         gameObject.GetComponent<Image>().color = colorBiome;
