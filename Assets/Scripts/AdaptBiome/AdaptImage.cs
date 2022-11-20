465e3208ada0fcdad270f779123f963677b073b1
Fromiel <79905976+Fromiel@users.noreply.github.com>
2022-11-19 22:16:04 -0500

    couleurs

--- Assets/Scripts/AdaptBiome/AdaptImage.cs
+++ Assets/Scripts/AdaptBiome/AdaptImage.cs
@@ -14,13 +14,13 @@ public class AdaptImage : MonoBehaviour
                 colorBiome = new Color(0.76f, 0.12f, 0.22f);
                 break;
             case 1:
-                colorBiome =new Color(0.76f, 0.12f, 0.22f);
+                colorBiome =new Color(0.05f, 0.76f, 0.42f);
                 break;
             case 2:
-                colorBiome = new Color(0.76f, 0.12f, 0.22f);
+                colorBiome = new Color(0.2f, 0.76f, 0.7f);
                 break;
             case 3:
-                colorBiome = new Color(0.76f, 0.12f, 0.22f);
+                colorBiome = new Color(0.76f, 0.34f, 0.46f);
                 break;
         }
         gameObject.GetComponent<Image>().color = colorBiome;
