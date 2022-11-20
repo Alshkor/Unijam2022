49cf4b44ddeb46b6265438fffcb0cba1e707314f
Alshkor <72515533+Alshkor@users.noreply.github.com>
2022-11-20 04:59:05 +0100

    codeur professionnel

--- Assets/Scripts/Animations/ShopAnimation.cs
+++ Assets/Scripts/Animations/ShopAnimation.cs
@@ -17,22 +17,26 @@ public class ShopAnimation : MonoBehaviour
         _system = GetComponent<ParticleSystem>();
         ParticleSystem.MainModule main = _system.main;
         Color col = GameManager.Instance.getColor();
-        main.startColor = new ParticleSystem.MinMaxGradient(col);
+
         _renderer = GetComponent<ParticleSystemRenderer>();
         
         switch (GameManager.Instance.biome)
         {  
             case 0:
                 _renderer.mesh = orange;
+                main.startColor = new ParticleSystem.MinMaxGradient(new Color(196,156,0));
                 break;
             case 1: 
                 _renderer.mesh = vert;
+                main.startColor = new ParticleSystem.MinMaxGradient(new Color(	30,162,0));
                 break;
             case 2:
                 _renderer.mesh = blue;
+                main.startColor = new ParticleSystem.MinMaxGradient(new Color(0,40,	162));
                 break;
             case 3:
                 _renderer.mesh = murasaki;
+                main.startColor = new ParticleSystem.MinMaxGradient(new Color(	151,0,119));
                 break;
         }
     }
