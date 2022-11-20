d6c261deb7d16d570c24ac1eadd0c9f25cece081
Alshkor <72515533+Alshkor@users.noreply.github.com>
2022-11-20 01:20:59 +0100

    player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,15 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(PlayerMovement))]
 public class Player : MonoBehaviour
 {
     #region PrivateAttribute
-    
 
+    [SerializeField] private UnityEvent onDeath;
     #endregion
     
     #region Monobehavior Callback
@@ -28,6 +30,13 @@ public class Player : MonoBehaviour
 
     public void KillPlayer()
     {
+        onDeath.Invoke();
+        StartCoroutine(DestroyAfter(2));
+    }
+
+    IEnumerator DestroyAfter(float time)
+    {
+        yield return new WaitForSeconds(time);
         GameManager.Instance.Lose();
     }
     
